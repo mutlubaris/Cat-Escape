1b570c1986d50f9a82ed01ca3fbd9aa4724c7bd2
Baris Mutlu <mutlubaris_1@hotmail.com>
2023-03-07 16:37:03 +0300

    human motion

--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,28 @@ public class HumanController : MonoBehaviour
 {
     [SerializeField] private Image _sensorImage;
     [SerializeField] private float _sensorAngle = 90;
+    [SerializeField] private float _idleDuration = 1f;
+    [SerializeField] private float _moveDistance = 5f;
+    [SerializeField] private float _moveDuration = 2f;
+    [SerializeField] private float _rotateDuration = 1f;
+
+    private Sequence sequence;
     
     private void Start()
     {
         _sensorImage.transform.localRotation= Quaternion.identity;
         _sensorImage.transform.Rotate(new Vector3(0, 0, _sensorAngle / 2));
         _sensorImage.fillAmount = _sensorAngle / 360;
+
+        sequence = DOTween.Sequence();
+        sequence.Append(transform.DOMove(transform.position + transform.forward * _moveDistance, _moveDuration));
+        sequence.AppendInterval(_idleDuration);
+        sequence.Append(transform.DORotate(transform.rotation.eulerAngles + new Vector3(0, 180, 0), _rotateDuration));
+        sequence.Append(transform.DOMove(transform.position, _moveDuration));
+        sequence.AppendInterval(_idleDuration);
+        sequence.Append(transform.DORotate(transform.rotation.eulerAngles, _rotateDuration));
+
+        sequence.SetLoops(-1);
     }
 
     private void OnTriggerStay(Collider other)
@@ -21,7 +38,7 @@ public class HumanController : MonoBehaviour
         {
             var angleBetweenSelfAndCat = Vector2.Angle(new Vector2(transform.forward.x, transform.forward.z), 
                 new Vector2(other.transform.position.x - transform.position.x, other.transform.position.z - transform.position.z));
-            if (angleBetweenSelfAndCat < _sensorAngle / 2) print("Caught");
+            if (angleBetweenSelfAndCat < _sensorAngle / 2) sequence.Kill();
         }
     }
 }
